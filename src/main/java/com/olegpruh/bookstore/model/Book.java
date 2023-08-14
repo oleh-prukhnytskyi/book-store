729cf32f8598a4f2553ac6c5421dedbb77653a66
Oleg Pruh <olegpruh1@gmail.com>
2023-08-14 23:13:34 +0100

    infrastructure commit

--- src/main/java/com/olegpruh/bookstore/model/Book.java
+++ src/main/java/com/olegpruh/bookstore/model/Book.java
@@ -1,14 +1,12 @@
 package com.olegpruh.bookstore.model;
 
+import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import lombok.Data;
-
-import java.math.BigDecimal;
 
 @Data
 @Entity
