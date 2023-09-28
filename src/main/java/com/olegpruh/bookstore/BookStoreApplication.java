ba7a3c39edcfa75914fdef75d542095610f6a8b3
Oleg Pruh <olegpruh1@gmail.com>
2023-09-28 12:13:57 +0100

    Revert "infrastructure commit"
    This reverts commit 729cf32f8598a4f2553ac6c5421dedbb77653a66.

--- src/main/java/com/olegpruh/bookstore/BookStoreApplication.java
+++ src/main/java/com/olegpruh/bookstore/BookStoreApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookStoreApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BookStoreApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
 }
--- src/main/java/com/olegpruh/bookstore/service/BookService.java
+++ src/main/java/com/olegpruh/bookstore/service/BookService.java
@@ -1,10 +1,10 @@
 package com.olegpruh.bookstore.service;
 
 import com.olegpruh.bookstore.model.Book;
+
 import java.util.List;
 
 public interface BookService {
     Book save(Book book);
-
     List<Book> findAll();
 }
