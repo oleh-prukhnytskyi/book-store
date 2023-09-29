2677fa31c122a253de71c9950f138eb3981a0c4b
Oleg Pruh <olegpruh1@gmail.com>
2023-09-29 16:25:37 +0100

    Redesigned findBookById method in book service.

--- src/main/java/com/olegpruh/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/olegpruh/bookstore/service/impl/BookServiceImpl.java
@@ -37,9 +37,9 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto findBookById(Long id) {
-        Book book = bookRepository.findById(id)
+        return bookRepository.findById(id)
+                .map(bookMapper::toDto)
                 .orElseThrow(() -> new EntityNotFoundException("Can't find book by id: " + id));
-        return bookMapper.toDto(book);
     }
 
     @Override
