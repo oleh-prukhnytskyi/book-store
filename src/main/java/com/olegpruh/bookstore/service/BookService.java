979d2705b36ffaf5e390eede2306901cd829afc7
Oleg Pruh <olegpruh1@gmail.com>
2023-10-03 13:33:55 +0100

    Added swagger, sorting and pagination support.

--- src/main/java/com/olegpruh/bookstore/service/BookService.java
+++ src/main/java/com/olegpruh/bookstore/service/BookService.java
@@ -4,11 +4,12 @@ import com.olegpruh.bookstore.dto.BookSearchParametersDto;
 import com.olegpruh.bookstore.dto.request.CreateBookRequestDto;
 import com.olegpruh.bookstore.dto.response.BookDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findBookById(Long id);
 
